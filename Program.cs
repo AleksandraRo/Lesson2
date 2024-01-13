434a57fc0b8ad599bed0965a5fd4fa02e654b9dd
Aleksandra Rogunova <79119954295@yandex.ru>
2024-01-13 16:16:09 +0300

    сохранено для выпуска

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int n = 5;
+int [] array = { 1, 4, 7, 13, 13 };
+// int i = 0;
+// int max = array[0];
+// while(i < n)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+
+// max = array[0];
+// for (int j = 0; j < n; j++)
+// {
+//     if (array[j] > max)
+//     {
+//         max = array[j];
+//     }
+// }
+// Console.WriteLine(max);   
+
+
+int max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);   
